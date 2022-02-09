aaa51277c82d69ff26e137e44c615c8f74ccdd71
Aaryaman1409 <59561784+Aaryaman-1409@users.noreply.github.com>
2022-02-09 20:36:19 +0000

    Update settings.py

--- djangoPerm/settings.py
+++ djangoPerm/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-_@9x!3b8o&(j(!r5t_@8uzqx)#%hyu##)!84ubs1(z-49(_u3l'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
