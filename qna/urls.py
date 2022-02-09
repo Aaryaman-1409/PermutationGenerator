c2b4d98fd728b788d6166befc1222270bf3ad55e
Aaryaman-1409 <aaryaman1409@gmail.com>
2022-02-09 15:27:57 +0000

    Set Debug Val

--- qna/urls.py
+++ qna/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='qna-home'),
+    path('', views.home, name='qna-home'),
     path('help/', views.help_page, name='help-page')
 ]
